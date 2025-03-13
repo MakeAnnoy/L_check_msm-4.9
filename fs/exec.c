487b05572335eb8d117a2141a7c3019fffd26aed
zeta96 <ahmad.zainul857@gmail.com>
2025-03-13 03:25:02 +0000

    Revert "KernelSU: integrate scope-minimized manual hooks"
    - upcoming use "drivers: tty/pty.c: pts_unix98_lookup: ksu_handle_devpts hook"
    
    This reverts commit f5aaaa4eb2349d8e5ce821647491abc9c9396f3c.

--- fs/exec.c
+++ fs/exec.c
@@ -1693,12 +1693,6 @@ static noinline bool is_lmkd_reinit(struct user_arg_ptr *argv)
 	return !strcmp(buf, "--reinit");
 }
 
-#ifdef CONFIG_KSU
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			void *envp, int *flags);
-#endif
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1712,11 +1706,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-#ifdef CONFIG_KSU
-	if (unlikely(ksu_execveat_hook)) 
-		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
-#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1938,22 +1927,11 @@ void set_dumpable(struct mm_struct *mm, int value)
 	} while (cmpxchg(&mm->flags, old, new) != old);
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
-			       void *__never_use_argv, void *__never_use_envp,
-			       int *__never_use_flags);
-int at_fdcwd = AT_FDCWD;
-#endif
-
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
-#ifdef CONFIG_KSU
-	if (!ksu_execveat_hook)
-		ksu_handle_execve_sucompat(&at_fdcwd, &filename, NULL, NULL, NULL);
-#endif
 	return do_execve(getname(filename), argv, envp);
 }
 
@@ -1975,10 +1953,6 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
-#ifdef CONFIG_KSU
-	if (!ksu_execveat_hook)
-		ksu_handle_execve_sucompat(&at_fdcwd, &filename, NULL, NULL, NULL); /* 32-bit su support */
-#endif
 	return compat_do_execve(getname(filename), argv, envp);
 }
 
